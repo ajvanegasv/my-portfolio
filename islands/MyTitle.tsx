a3ac0982e2df80ff019f38d7943254e832a9d9c0
Alvaro J Vanegas <alvarovanegasv28@gmail.com>
2023-03-01 14:55:22 +0000

    se corrige el llamado de greetingType, ahora no es una constante, se hace desde las props

--- islands/MyTitle.tsx
+++ islands/MyTitle.tsx
@@ -3,19 +3,18 @@ import { useEffect, useState } from "preact/hooks";
 import TitleTyping from "../components/titles/TitleTyping.tsx";
 
 export default function MyTitle(props: { name: string; greetingType: string }) {
-  const greetingType = "I'am";
   const [greeting, setGreeting] = useState("");
   const [name, setName] = useState("");
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      setGreeting(greetingType.slice(0, greeting.length + 1));
+      setGreeting(props.greetingType.slice(0, greeting.length + 1));
     }, 150);
     return () => clearTimeout(timeout);
   }, [greeting]);
 
   useEffect(() => {
-    if (greetingType.length === greeting.length) {
+    if (props.greetingType.length === greeting.length) {
       const timeout = setTimeout(() => {
         setName(props.name.slice(0, name.length + 1));
       }, 200);
@@ -23,5 +22,5 @@ export default function MyTitle(props: { name: string; greetingType: string }) {
     }
   }, [name, greeting]);
 
-  return <TitleTyping name={name} greeting={greeting} greetingLength={greetingType.length} />;
+  return <TitleTyping name={name} greeting={greeting} greetingLength={props.greetingType.length} />;
 }
