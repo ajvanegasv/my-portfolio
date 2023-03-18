29b57562b720e49b064c2c36eb0059ee0eb6b240
Alvaro J Vanegas <alvarovanegasv28@gmail.com>
2023-03-18 23:39:28 +0000

    add meta tags

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,4 @@
-import { Head, asset } from "$fresh/runtime.ts";
+import { asset, Head } from "$fresh/runtime.ts";
 import { AppProps } from "$fresh/server.ts";
 import { tw } from "twind";
 
@@ -11,8 +11,19 @@ export default function App({ Component }: AppProps) {
         <title>Alvaro J Vanegas - Full Stack Dev</title>
         <meta
           name="description"
-          content="Alvaro J Vanegas - Software Development website"
+          content="Alvaro J Vanegas - Software Developer website"
         />
+        <meta property="og:url" content="https://ajvanegasv.deno.dev/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Alvaro J Vanegas" />
+        <meta property="og:description" content="Alvaro J Vanegas Website" />
+        <meta property="og:image" content={asset("assets/socialcard.png")} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="ajvanegasv.deno.dev" />
+        <meta property="twitter:url" content="https://ajvanegasv.deno.dev/" />
+        <meta name="twitter:title" content="Alvaro J Vanegas" />
+        <meta name="twitter:description" content="Alvaro J Vanegas Website" />
+        <meta name="twitter:image" content={asset("assets/socialcard.png")} />
       </Head>
       <body id="app" class={tw`bg-pallete-primary ${globalStyles}"`}>
         <Component />
